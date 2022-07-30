73a6f71f88224e556bdab6571da6c38aab01b4b9
Gaetano Di Russo <dirussogaetano@gmail.com>
2022-07-30 10:23:00 -0300

    local files provider added

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,30 +1,4 @@
 module.exports = [
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'dl.airtable.com',
-            'gaetano-portfolio.s3.sa-east-1.amazonaws.com',
-          ],
-          'media-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'dl.airtable.com',
-            'gaetano-portfolio.s3.sa-east-1.amazonaws.com',
-          ],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
   'strapi::errors',
   'strapi::security',
   'strapi::cors',
